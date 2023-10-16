b1d116a6bad5d8e87e9dad031a3d83c9feb81882
Thanh-HaiNguyen <nguyenhaithanh.uit@gmail.com>
2023-10-16 10:50:52 +0700

    Refator iam modules

--- modules/iam-module/variables.tf
+++ modules/iam-module/variables.tf
@@ -3,17 +3,17 @@ variable "iam_role_name" {
   description = "IAM role name"
 }
 
-variable "iam_role_type" {
-  type = string
-  description = "IAM role type"
+variable "iam_role_assume" {
+  type        = string
+  description = "IAM role assume"
 }
 
-variable "iam_policy_name" {
+variable "iam_role_policy_name" {
   type        = string
   description = "IAM policy name"
 }
 
-variable "iam_policy" {
+variable "iam_role_policy" {
   type        = string
   description = "IAM policy"
 }
